5f822f299a4afae9494c1740749eb5d059776486
lkotlewski <luckyyplayer@gmail.com>
2020-04-25 12:59:01 +0200

    Polish wiki context match dataset and processor added

--- run_classifier.py
+++ run_classifier.py
@@ -374,6 +374,42 @@ class ColaProcessor(DataProcessor):
     return examples
 
 
+class PlwiProcessor(DataProcessor):
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+    def get_test_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+
+    def get_labels(self):
+        """See base class."""
+        return ["true", "false"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            if i == 0:
+                continue
+            guid = "%s-%s" % (set_type, i)
+            print(guid)
+            text_a = tokenization.convert_to_unicode(line[4])
+            text_b = tokenization.convert_to_unicode(line[5])
+            label = tokenization.convert_to_unicode(line[6])
+            examples.append(
+                InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
+        return examples
+
 def convert_single_example(ex_index, example, label_list, max_seq_length,
                            tokenizer):
   """Converts a single `InputExample` into a single `InputFeatures`."""
@@ -788,6 +824,7 @@ def main(_):
       "mnli": MnliProcessor,
       "mrpc": MrpcProcessor,
       "xnli": XnliProcessor,
+      'plwi': PlwiProcessor
   }
 
   tokenization.validate_case_matches_checkpoint(FLAGS.do_lower_case,
